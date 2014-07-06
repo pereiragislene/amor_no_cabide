63804335b31f9e7d36c98bd63bbe0a4163fe1ece
Gislene Pereira <gislene01@gmail.com>
2014-07-06 18:11:04 -0300

    Removing unecessary migrations

--- db/migrate/20140705024551_add_latitude_to_ponto.rb
+++
@@ -1,5 +0,0 @@
-class AddLatitudeToPonto < ActiveRecord::Migration
-  def change
-    add_column :pontos, :latitude, :float
-  end
-end
--- db/migrate/20140705024604_add_longitude_to_ponto.rb
+++
@@ -1,5 +0,0 @@
-class AddLongitudeToPonto < ActiveRecord::Migration
-  def change
-    add_column :pontos, :longitude, :float
-  end
-end
