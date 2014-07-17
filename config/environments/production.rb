bc72baee2d3d00f0f759645541bd583727272d25
Gislene Pereira <gislene01@gmail.com>
2014-07-16 22:13:13 -0300

    Fixing css conflicts with Active Admin. Compiling assets. Changing navbar back to clear version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += ['active_admin.css']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
